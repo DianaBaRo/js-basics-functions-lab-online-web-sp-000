1754cd15a91e02d361e77e02733efb046b10f9ec
Diana Badas <diana.badas@gmail.com>
2019-07-24 22:34:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  if (pickupLocation > 42) {
+    return pickupLocation - 42;
+  } else {
+    return 42 - pickupLocation;
+  }
+};
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+};
+
+function distanceTravelledInFeet() {
+
+};
+
+function calculatesFarePrice(start, destination) {
+
+};
