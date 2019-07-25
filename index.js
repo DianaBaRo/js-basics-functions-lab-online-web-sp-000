1405579f404260813805f0ea94ebfe62a611d1cd
Diana Badas <diana.badas@gmail.com>
2019-07-25 13:22:58 +0000

    Done.

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ function distanceFromHqInBlocks(pickupLocation) {
   } else {
     return 42 - pickupLocation;
   }
-};
+}
 
 function distanceFromHqInFeet(pickupLocation) {
   return distanceFromHqInBlocks(pickupLocation) * 264;
-};
+}
 
-function distanceTravelledInFeet() {
-
-};
+function distanceTravelledInFeet(pickupLocation, endingBlock) {
+  if (pickupLocation < endingBlock) {
+    return (endingBlock - pickupLocation) * 264;
+  } else {
+    return (pickupLocation - endingBlock) * 264;
+  }
+}
 
 function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
 
-};
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
